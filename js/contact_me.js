74344f92185017cc69bd7a7ec6d48a2272842db7
NickCopelin <nickcopelin@Nicks-MacBook-Air.local>
2014-08-21 16:11:57 -0500

    Contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//forms.brace.io/nickcopelin@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
